1394f3619e3b4fe327d5dd4f708d612e9228e331
amir <amirfirdausoff@gmail.com>
2020-09-30 17:23:35 +0800

    - add staging and debug version

--- app/src/main/java/com/bateriku/bplazmerchant/Connection/BasedURL.java
+++ app/src/main/java/com/bateriku/bplazmerchant/Connection/BasedURL.java
@@ -1,7 +1,26 @@
 package com.bateriku.bplazmerchant.Connection;
 
-public class BasedURL {
-    public static final String ROOT_URL = "https://merchant.bplaz.com/api/v1/";
-    public static final String ROOT_URL_IMAGE = "https://merchant.bplaz.com/";
+import android.app.Application;
+import android.content.Context;
+
+import com.bateriku.bplazmerchant.R;
+
+public class BasedURL extends Application {
+
+    private static Context mContext;
+    public static String ROOT_URL;
+    public static String ROOT_URL_IMAGE;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mContext = this;
+
+        ROOT_URL = getString(R.string.ROOT_URL);
+        ROOT_URL_IMAGE = getString(R.string.ROOT_URL_IMAGE);
+    }
+
+    public static Context getContext(){
+        return mContext;
+    }
 
 }
